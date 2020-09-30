d604dfa8b9385f6224b04d6a21980d0bab6378e0
carl.jin <carl.jin@airwallex.com>
2020-09-30 15:03:43 +0800

    feat: add dotenv

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 const semanticRelease = require('semantic-release');
 
 const run = async () => {
+  console.log(process.env)
   const result = await semanticRelease({
     branches: ['master'],
     repositoryUrl: 'https://github.com/carl-jin-awx/utils.git',
     dryRun: false,
     ci: false,
   }, {
-    env: {...process.env, GH_TOKEN: 'c307a1c799d6b8c78f5c0b80c76eca10d0e89d1e'}
+    env: process.env
   });
   if (result) {
     console.log(result.lastRelease);
