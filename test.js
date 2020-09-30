8d896e221d2002db14c533e377250724a507575a
carl.jin <carl.jin@airwallex.com>
2020-09-30 14:52:08 +0800

    feat: update

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const semanticRelease = require('semantic-release');
 const run = async () => {
   const result = await semanticRelease({
     branches: ['master'],
-    repositoryUrl: 'git@github.com:carl-jin-awx/utils.git',
+    repositoryUrl: 'https://github.com/carl-jin-awx/utils.git',
     dryRun: false,
     ci: false,
   }, {
-    env: {...process.env, GH_TOKEN: '3a1fbdaa2313f77a43760f7f46e53b7f363ae9aa'}
+    env: {...process.env, GH_TOKEN: 'c307a1c799d6b8c78f5c0b80c76eca10d0e89d1e'}
   });
   if (result) {
     console.log(result.lastRelease);
