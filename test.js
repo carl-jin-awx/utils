c17fd50610562b1affcc24a1d852ba42c98a57b0
carl.jin <carl.jin@airwallex.com>
2020-09-30 14:34:23 +0800

    feat: add token

--- test.js
+++ test.js
@@ -4,6 +4,10 @@ const run = async () => {
   const result = await semanticRelease({
     branches: ['master'],
     repositoryUrl: 'git@github.com:carl-jin-awx/utils.git',
+    dryRun: false,
+    ci: false,
+  }, {
+    env: {...process.env, GH_TOKEN: '3a1fbdaa2313f77a43760f7f46e53b7f363ae9aa'}
   });
   if (result) {
     console.log(result.lastRelease);
